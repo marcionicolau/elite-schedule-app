e0d632ad6b6d41ff4e39c9db6baa8e7b5bec5d15
Marcio Nicolau <nicolau.3g@gmail.com>
2017-01-05 18:22:12 -0200

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { MyTeamsPage } from './../pages/my-teams/my-teams.page';
+import { TournamentsPage } from './../pages/tournaments/tournaments';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    MyTeamsPage,
+    TournamentsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    MyTeamsPage,
+    TournamentsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/pages/my-teams/my-teams.page.ts
@@ -0,0 +1,13 @@
+import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'build/pages/my-teams/my-teams.page.html'
+})
+
+export class MyTeamsPage {
+
+    constructor(private navCtrl: NavController){
+        
+    }
+}
